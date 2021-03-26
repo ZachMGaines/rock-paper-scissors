b53b1e64273a37a67efe4a4e1120c7957a68d87b
Zachary Gaines <zacharygaines28@gmail.com>
2021-03-26 16:37:20 -0700

    function testing

--- test/example.test.js
+++ test/example.test.js
@@ -1,16 +1,43 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
 
-import { getComputersThrow } from '../utils.js';
+import { whoWin } from '../utils.js';
 
 const test = QUnit.test;
 
-test('If i have rock that should equal 1', (expect) => {
-    const expected = 'rock';
-
-
-    const actual = getComputersThrow(1);
-
-
+test('If i have rock and computer has paper', (expect) => {
+    const expected = 'loss';
+    const actual = whoWin('rock', 'paper');
     expect.equal(actual, expected);
 });
+
+test('If i have rock and computer has rock, we should tie', (expect) => {
+    const expected = 'tie';
+    const actual = whoWin('rock', 'rock');
+    expect.equal(actual, expected);
+});
+
+test('If i have rock and computer has scissors, user should win', (expect) => {
+    const expected = 'win';
+    const actual = whoWin('rock', 'scissors');
+    expect.equal(actual, expected);
+});
+
+test('If i have paper and computer has rock, user should win', (expect) => {
+    const expected = 'win';
+    const actual = whoWin('paper', 'rock');
+    expect.equal(actual, expected);
+});
+
+test('If i have paper and computer has paper, user should win', (expect) => {
+    const expected = 'tie';
+    const actual = whoWin('paper', 'paper');
+    expect.equal(actual, expected);
+});
+
+test('If i have paper and computer has scissors, user should lose', (expect) => {
+    const expected = 'loss';
+    const actual = whoWin('paper', 'scissors');
+    expect.equal(actual, expected);
+});
+
--- utils.js
+++ utils.js
@@ -1,19 +1,4 @@
-/*export function getComputersThrow() {
-    let randomNumber = Math.ceil(Math.random()) * 3;
-    let handShape;
 
-    if (randomNumber === 1) {
-        handShape = 'rock';
-    }
-    if (randomNumber === 2) {
-        handShape = 'paper';
-    }
-    if (randomNumber === 3) {
-        handShape = 'scissors';
-
-    }
-    return handShape;
-} */
 export function jankan() {
     const randomNumber = Math.ceil(Math.random() * 3);
     if (randomNumber === 1) return 'rock';
